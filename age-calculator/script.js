a5a6511827ecd34dbb7962060059a93ee9353826
Rajat7ku <rajatkumar52557@gmail.com>
2026-06-23 08:46:10 +0530

    complete my age calculator project

--- age-calculator/script.js
+++ age-calculator/script.js
@@ -6,8 +6,39 @@ const h2 = document.querySelector("h2");
 
 btn.addEventListener("click", ()=>{
     const currDate = new Date();
+    console.log(currDate);
     
     let dobValue = user_date.value;
-    console.log(dobValue);
+
+
+    if(dobValue === ""){
+        h2.innerText = "Please enter the DOB";
+        h2.style.color = "red";
+        return;
+    }
+
+    h2.style.color = "black";
+
+    const birthDOB = new Date(dobValue);
+
+    let currYear = currDate.getFullYear();
+    let currMonth = currDate.getMonth();
+    let currDay = currDate.getDate();
+
+    let birthYear = birthDOB.getFullYear();
+    let birthMonth = birthDOB.getMonth();
+    let birthDay = birthDOB.getDate();
+    
+
+    let age = currYear - birthYear;
+
+    console.log(age);
+
+    if(currMonth <birthMonth || (currMonth === birthMonth && currDay < birthDay)){
+        age--;
+    }
+
+    h2.innerText  = `your ${age} is this`
+    
     
 })
